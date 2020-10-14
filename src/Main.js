342ccf466a7f3f2ac96539558f48bc68711562ea
iogod <johnson.roscoea@gmail.com>
2020-10-14 17:11:49 -0400

    Faster Main Render

--- src/Main.js
+++ src/Main.js
@@ -1,28 +1,41 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Skyline from "./media/airplane.jpeg";
+import "./Main.css";
 
-
-import Skyline from './media/airplane.jpeg'
-import  './Main.css'
-
+let Photo = () => {
+  return (
+    <img
+      onLoad={"callbackAfterImageIsDownloaded"}
+      style={{ width: "100%", height: "100%", position: "fixed" }}
+      src={Skyline}
+      alt="no"
+    ></img>
+  );
+};
 
 export class Main extends Component {
-  
-    render() {
-        return (
-            <div className = "lander">
-    
-      <img  style = {{width: '100%', height:'100%' , position : "fixed",}} src = {Skyline} alt = "no"></img>
-      {/* <h1 style= {{top:'20%'}}>Roscoe Johnson</h1> */}
-     
-      <h5>A journey of a thousand miles begins with a single step</h5>
-      <div  style = {{backgroundColor:"red",position:"absolute",top:"50%",right:"100%",width:"200px",height:"200px",borderRadius:"50%",zIndex:"20"}}>
-<h1> hi</h1>
+  render() {
+    return (
+      <div className="lander">
+        <Photo />
+        <h5>A journey of a thousand miles begins with a single step</h5>
+        <div
+          style={{
+            backgroundColor: "red",
+            position: "absolute",
+            top: "50%",
+            right: "100%",
+            width: "200px",
+            height: "200px",
+            borderRadius: "50%",
+            zIndex: "20",
+          }}
+        >
+          <h1> hi</h1>
+        </div>
       </div>
-     
-
-            </div>
-        )
-    }
+    );
+  }
 }
 
-export default Main
+export default Main;
