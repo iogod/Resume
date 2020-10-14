e6197b783f519f9b9e4804e99b06342438adec01
iogod <johnson.roscoea@gmail.com>
2020-10-14 17:37:56 -0400

    Updates

---
+++ precache-manifest.62d948dc83b09540c8a670d65cbb5cde.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8042d2eff8c3bc3cc6a3964db4b95ed",
+    "url": "/Resume/index.html"
+  },
+  {
+    "revision": "0b36c444e019fd7582e3",
+    "url": "/Resume/static/css/main.fa72d1ca.chunk.css"
+  },
+  {
+    "revision": "f3ac2d976581ef3d1653",
+    "url": "/Resume/static/js/2.47e059df.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Resume/static/js/2.47e059df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b36c444e019fd7582e3",
+    "url": "/Resume/static/js/main.288499e9.chunk.js"
+  },
+  {
+    "revision": "6937ac26d5bf496be4aa",
+    "url": "/Resume/static/js/runtime-main.4f7f43b1.js"
+  },
+  {
+    "revision": "4e2792090107c98db4095fe5d2fab017",
+    "url": "/Resume/static/media/USC.4e279209.jpg"
+  },
+  {
+    "revision": "769fc7770219223ac293979d12af528d",
+    "url": "/Resume/static/media/airplane.769fc777.png"
+  },
+  {
+    "revision": "96ae185f5027d8bf9384c19bd35af3e0",
+    "url": "/Resume/static/media/ddg.96ae185f.jpg"
+  },
+  {
+    "revision": "7c35d4007c437713817429048563d452",
+    "url": "/Resume/static/media/gps.7c35d400.jpg"
+  },
+  {
+    "revision": "4952e05b83977abe58ef8a8fbd824f31",
+    "url": "/Resume/static/media/image1.4952e05b.jpeg"
+  },
+  {
+    "revision": "932f0193d9df97ac0f279c282e5cbea8",
+    "url": "/Resume/static/media/linkedin.932f0193.jpg"
+  },
+  {
+    "revision": "f704edbe8dbdae384356a3de2ff9ea01",
+    "url": "/Resume/static/media/morganstate.f704edbe.jpg"
+  },
+  {
+    "revision": "fa31dcac353bf972e463f1d08a2febba",
+    "url": "/Resume/static/media/mountain.fa31dcac.png"
+  }
+]);
