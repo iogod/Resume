22bf043263f1a57266691bd09d891cb6ec4e43fe
iogod <johnson.roscoea@gmail.com>
2020-10-16 17:24:07 -0400

    titlechange

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -11,7 +11,7 @@ export class contact extends Component {
   render() {
     return (
       <div>
-        <div className="bio" style={{ paddingTop: "20%" }}>
+        <div className="bio" style={{ paddingTop: "7%" }}>
           <h1>Contact Me </h1>
         </div>
 
